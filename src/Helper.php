f3f00c116c84558bcb1a2f96b9f31445c7443276
kuangjy2 <kuangjy@qq.com>
2017-09-09 05:26:51 +0800

    补phpdoc注释

--- src/Helper.php
+++ src/Helper.php
@@ -5,6 +5,11 @@ namespace Omnipay\AyPay;
 class Helper
 {
 
+    /**
+     * @param array $arr
+     * @param string $root
+     * @return string
+     */
     public static function array2xml($arr, $root = 'xml')
     {
         $xml = "<$root>";
@@ -21,14 +26,21 @@ class Helper
         return $xml;
     }
 
-
+    /**
+     * @param string $xml
+     * @return mixed
+     */
     public static function xml2array($xml)
     {
         $xml = str_replace("&", "{xml}", $xml);
         return json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
     }
 
-
+    /**
+     * @param array $data
+     * @param string $key
+     * @return string
+     */
     public static function sign($data, $key)
     {
         unset($data['sign']);
